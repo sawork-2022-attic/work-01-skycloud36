2b776be825e024474c0fd505ac1c09f1ebf88f3b
skycloud36 <1076626296@qq.com>
2022-03-07 03:15:39 +0800

    annotation

---
+++ asciipanel-annotation/src/main/java/com/skycloud/Main.java
@@ -0,0 +1,34 @@
+package com.skycloud;
+import com.skycloud.asciiPanel.AsciiFont;
+import com.skycloud.asciiPanel.AsciiPanel;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config-annotation.xml");
+        AsciiFont asciiFont = context.getBean(AsciiFont.class);
+        AsciiFont taffer = AsciiFont.TAFFER_10x10;
+        System.out.println("AsciiFont_Default: " + asciiFont.getFontFilename() + " "
+                + asciiFont.getWidth() + " " + asciiFont.getHeight());
+        System.out.println("AsciiFont_TAFFER: " + taffer.getFontFilename() + " "
+                + taffer.getWidth() + " " + taffer.getHeight() );
+        AsciiPanel asciiPanel = context.getBean(AsciiPanel.class);
+        System.out.println("AsciiPanel_Default: " + asciiPanel.getAsciiFont().getFontFilename() + " "
+                + asciiPanel.getWidthInCharacters() + " " + asciiPanel.getHeightInCharacters());
+        AsciiPanel taff = new AsciiPanel(80, 24, taffer);
+        System.out.println("Asciipanel_TAFFER: " + taff.getAsciiFont().getFontFilename() + " "
+                + taff.getWidthInCharacters() + " " + taff.getHeightInCharacters());
+
+
+        JFrame jFrame = new JFrame();
+        jFrame.add(asciiPanel);
+        jFrame.pack();
+        asciiPanel.write("Welcome to Software Architecture!");
+        jFrame.setVisible(true);
+    }
+}
